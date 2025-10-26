700087d916c316de65b5d8df8e669faf79c7e590
Krishan-R <krishan.raithatha274@gmail.com>
2025-10-26 15:08:39 +0000

    updated bean mapper

--- AllTheBeans/JsonBeanMapper.cs
+++ AllTheBeans/JsonBeanMapper.cs
@@ -16,11 +16,22 @@ public static class JsonBeanMapper
 
         var countries = MapCountries(jsonBeans);
         var colours = MapColours(jsonBeans);
+        var beans = MapBeans(jsonBeans, countries, colours);
+        var beanOfTheDay = MapBeanOfTheDay(jsonBeans);
 
-        return ([], [], colours, countries);
+        return (beans, [], colours, countries);
+        // return ([], [], colours, countries);
     }
 
-    private static IEnumerable<Colour> MapColours(List<JsonBean> jsonBeans)
+    private static IEnumerable<Country> MapCountries(IEnumerable<JsonBean> jsonBeans)
+    {
+        var countries = jsonBeans.Select(x => x.Country).Distinct()
+            .Select((y, index) => new Country { CountryName = y, Id = index + 1});
+
+        return countries;
+    }
+
+    private static IEnumerable<Colour> MapColours(IEnumerable<JsonBean> jsonBeans)
     {
         var colours = jsonBeans.Select(x => x.colour).Distinct()
             .Select((y, index) => new Colour { ColourName = y, Id = index + 1});
@@ -28,11 +39,41 @@ public static class JsonBeanMapper
         return colours;
     }
 
-    private static IEnumerable<Country> MapCountries(List<JsonBean> jsonBeans)
+    private static IEnumerable<Bean> MapBeans(IEnumerable<JsonBean> jsonBeans, IEnumerable<Country> countries, IEnumerable<Colour> colours)
     {
-        var countries = jsonBeans.Select(x => x.Country).Distinct()
-            .Select((y, index) => new Country { CountryName = y, Id = index + 1});
+        var beans = jsonBeans.Select(x =>
+        {
+            var country = countries.First(y => y.CountryName == x.Country);
+            var colour = colours.First(y => y.ColourName == x.colour);
 
-        return countries;
+            return new Bean
+            {
+                Id = x._id,
+                Name = x.Name,
+                IsBOTD = x.isBOTD,
+                CostInGBP = float.Parse(x.Cost[1..]),
+                ImageUrl = x.Image,
+                Description = x.Description,
+                Country = country,
+                // CountryId = country.Id,
+                Colour = colour,
+                // ColourId = colour.Id
+            };
+        });
+
+        return beans;
+    }
+
+    private static IEnumerable<BeanOfTheDay> MapBeanOfTheDay(IEnumerable<JsonBean> jsonBeans)
+    {
+        var beanOfTheDay = jsonBeans.Where(x => x.isBOTD).Select((y, index) =>
+            new BeanOfTheDay
+            {
+                Id = index + 1,
+                Date = DateTime.UtcNow,
+                BeanId = y._id
+            });
+
+        return beanOfTheDay;
     }
 }
