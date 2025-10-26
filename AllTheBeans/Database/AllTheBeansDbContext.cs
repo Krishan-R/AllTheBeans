6eb4cdbb02fb3e9d1dfe9732dfe7ef4cf0baf846
Krishan-R <krishan.raithatha274@gmail.com>
2025-10-26 13:58:23 +0000

    cleanup

--- AllTheBeans/Database/AllTheBeansDbContext.cs
+++ AllTheBeans/Database/AllTheBeansDbContext.cs
@@ -5,6 +5,8 @@ namespace AllTheBeans.Database;
 
 public class AllTheBeansDbContext(DbContextOptions<AllTheBeansDbContext> options) : DbContext(options)
 {
+    private const string AllTheBeansJson = "AllTheBeans 1 (1).json";
+
     public DbSet<Bean> Beans { get; set; }
     public DbSet<BeanOfTheDay> BeanOfTheDay { get; set; }
     public DbSet<Colour> Colours { get; set; }
@@ -12,27 +14,24 @@ public class AllTheBeansDbContext(DbContextOptions<AllTheBeansDbContext> options
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        var json = File.ReadAllText("AllTheBeans 1 (1).json");
+        var json = File.ReadAllText(AllTheBeansJson);
         var (beans, beanOfTheDay, colours, countries) = JsonBeanMapper.MapFromJson(json);
 
         modelBuilder.Entity<Country>(builder =>
         {
             builder.HasIndex(c => c.CountryName).IsUnique();
-
             builder.HasData(countries);
         });
 
         modelBuilder.Entity<Colour>(builder =>
         {
             builder.HasIndex(c => c.ColourName).IsUnique();
-
             builder.HasData(colours);
         });
 
         modelBuilder.Entity<Bean>(builder =>
         {
             builder.HasIndex(b => b.Name).IsUnique();
-
             builder.HasData(beans);
         });
 
