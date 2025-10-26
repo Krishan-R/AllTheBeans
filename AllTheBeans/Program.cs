353c35e1742e2dac0a72f005430c596fec48dfa5
Krishan Raithatha <44500829+Krishan-R@users.noreply.github.com>
2025-10-26 11:15:38 +0000

    Adding initial template project (#1)
    * initial solution and project creation
    
    * converted program.cs to class
    
    * added swaggerˆ
    
    * fixing dockerfile
    
    * file cleanup

---
+++ AllTheBeans/Program.cs
@@ -0,0 +1,33 @@
+namespace AllTheBeans;
+
+internal abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
