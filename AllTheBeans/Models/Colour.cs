1228d12a60d30c0bae162828e08f637812aae8ba
Krishan-R <krishan.raithatha274@gmail.com>
2025-10-26 12:48:10 +0000

    created initial bean models

---
+++ AllTheBeans/Models/Colour.cs
@@ -0,0 +1,7 @@
+namespace AllTheBeans.Models;
+
+public class Colour
+{
+    public int Id { get; init; }
+    public string ColourName { get; init; }
+}
---
+++ AllTheBeans/Models/Country.cs
@@ -0,0 +1,7 @@
+namespace AllTheBeans.Models;
+
+public class Country
+{
+    public int Id { get; init; }
+    public required string CountryName { get; init; }
+}
