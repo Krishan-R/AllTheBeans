99ccdc6bedf86a9c6aed3955591fbe2d5f9964ac
Krishan-R <krishan.raithatha274@gmail.com>
2025-10-26 15:24:15 +0000

    added basic test

--- AllTheBeans.UnitTests/JsonBeanMapperTests.cs
+++ AllTheBeans.UnitTests/JsonBeanMapperTests.cs
@@ -14,5 +14,13 @@ public class JsonBeanMapperTests
     public void MapFromJson_CorrectlyReturnsMappedModels()
     {
         var (beans, beanOfTheDay, colours, countries) = JsonBeanMapper.MapFromJson(_json);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(beans.ToList(), Has.Count.EqualTo(15));
+            Assert.That(beanOfTheDay.ToList(), Has.Count.EqualTo(1));
+            Assert.That(colours.ToList(), Has.Count.EqualTo(5));
+            Assert.That(countries.ToList(), Has.Count.EqualTo(5));
+        });
     }
 }
