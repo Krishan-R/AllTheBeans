1d2a3a7b0c16f66091cde688c133ad5a3e709e80
Krishan-R <krishan.raithatha274@gmail.com>
2025-10-26 11:00:36 +0000

    added swaggerˆ

--- AllTheBeans/Program.cs
+++ AllTheBeans/Program.cs
@@ -8,12 +8,20 @@ internal abstract class Program
 
         builder.Services.AddControllers();
         builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
         }
 
         app.UseHttpsRedirection();
