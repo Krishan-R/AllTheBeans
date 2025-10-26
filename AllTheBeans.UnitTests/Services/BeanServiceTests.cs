b964d94554630fe90bfa56f9117d7203c6068188
Krishan-R <krishan.raithatha274@gmail.com>
2025-10-26 20:21:52 +0000

    updated unit tests

--- AllTheBeans.UnitTests/Services/BeanServiceTests.cs
+++ AllTheBeans.UnitTests/Services/BeanServiceTests.cs
@@ -1,3 +1,4 @@
+using AllTheBeans.Models;
 using AllTheBeans.Repositories;
 using AllTheBeans.Services;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -47,13 +48,74 @@ public class BeanServiceTests : BeanTestBase
     [Test]
     public async Task GetBeanOfTheDayAsync_CallsRepository()
     {
+        var beanOfTheDay = new BeanOfTheDay
+        {
+            Date = DateTime.UtcNow,
+            BeanId = _beans[0].Id,
+            Bean = _beans[0]
+        };
+
         _mockBeanRepository.Setup(x => x.GetBeanOfTheDayAsync())
-            .ReturnsAsync(_beans[0])
+            .ReturnsAsync(beanOfTheDay)
             .Verifiable(Times.Once);
 
         var result = await _beanService.GetBeanOfTheDayAsync();
 
         Assert.That(result, Is.EqualTo(_beans[0]));
         _mockBeanRepository.Verify();
+    }
+
+    [Test]
+    public async Task GetBeanOfTheDay_WhenRepositoryReturnsNull_GeneratesNewBeanOfTheDay()
+    {
+        var beanOfTheDay = new BeanOfTheDay
+        {
+            Date = DateTime.UtcNow,
+            BeanId = _beans[0].Id,
+            Bean = _beans[0]
+        };
+
+        var lastBeanOfTheDay = new BeanOfTheDay
+        {
+            Date = DateTime.UtcNow.AddDays(-1),
+            BeanId = _beans[1].Id,
+            Bean = _beans[1]
+        };
+
+        _mockBeanRepository.Setup(x => x.GetBeanOfTheDayAsync())
+            .ReturnsAsync((BeanOfTheDay)null!)
+            .Verifiable(Times.Once);
+
+        _mockBeanRepository.Setup(x => x.GetLastBeanOfTheDayAsync())
+            .ReturnsAsync(lastBeanOfTheDay)
+            .Verifiable(Times.Once);
+
+        _mockBeanRepository.Setup(x => x.GetAllBeansAsync())
+            .ReturnsAsync(_beans)
+            .Verifiable(Times.Once);
+
+        _mockBeanRepository.Setup(x => x.UpdateBeansAsync(It.IsAny<List<Bean>>()))
+            .Verifiable(Times.Once);
+
+        _mockBeanRepository.Setup(x => x.UpdateBeanAsync(It.Is<Bean>(b => b.IsBOTD == true)))
+            .Verifiable(Times.Once);
+
+        _mockBeanRepository.Setup(x => x.SetNewBeanOfTheDayAsync(It.IsAny<BeanOfTheDay>()))
+            .Callback(() =>
+            {
+                _mockBeanRepository.Setup(x => x.GetBeanOfTheDayAsync())
+                    .ReturnsAsync(beanOfTheDay)
+                    .Verifiable(Times.Once);
+            });
+
+        var result = await _beanService.GetBeanOfTheDayAsync();
+
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.IsBOTD, Is.True);
+            Assert.That(result.Id, Is.EqualTo(_beans[0].Id));
+            Assert.That(result.Id, Is.Not.EqualTo(lastBeanOfTheDay.Bean.Id));
+        });
     }
 }
